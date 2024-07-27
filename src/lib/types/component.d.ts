63b1930785b5f2adcbd86df2befb0c1eeb43a016
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-07-27 14:12:06 +0200

    🏷️ Add Component, library and framework badges

--- src/lib/types/component.d.ts
+++ src/lib/types/component.d.ts
@@ -5,3 +5,20 @@ export type VariantComponent = {
   component: React.JSX.Element;
   code: string;
 };
+
+export type ComponentBadge =
+  | "new"
+  | "updated"
+  | "deprecated"
+  | "experimental"
+  | "no-js";
+
+export type LibraryBadge = "framer-motion";
+
+export type FrameworkBadge =
+  | "react"
+  | "vue"
+  | "svelte"
+  | "angular"
+  | "web-components"
+  | "next";
