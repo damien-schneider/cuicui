b320ff1ecbdd868fdd225c8fa28a280c345d8370
Damien Schneider <damien.schneider01@gmail.com>
2024-07-28 02:34:35 +0200

    :sparkles: Add grid cursor component

--- src/app/(components)/other/cursors/dynamic-cards/variant2.tsx
+++ src/app/(components)/other/cursors/dynamic-cards/variant2.tsx
@@ -1,23 +1,24 @@
 "use client";
 import React, { useRef, useEffect, useState } from "react";
 
-export default function DynamicCardsVariant1() {
+export default function DynamicCardsVariant2() {
   return (
-    <div className="grid gap-2 grid-cols-1 md:grid-cols-2">
-      <Card
-        title="CuiCui"
-        content="This is the best library for creating dynamic cards"
-      />
-      <Card title="Just crazy" content="How fast is it now to create cards" />
-
-      <Card
-        title="Amazing"
-        content="I just have to copy paste the code and it instantly works"
-      />
-      <Card
-        title="Unbelievable"
-        content="I can't believe how easy it is to use"
-      />
+    <div className="relative w-full h-full">
+      <div className="grid gap-2 md:grid-cols-2 grid-cols-1">
+        <Card
+          title="CuiCui"
+          content="This is the best library for creating dynamic cards"
+        />
+        <Card title="Just crazy" content="How fast is it now to create cards" />
+        <Card
+          title="Amazing"
+          content="I just have to copy paste the code and it instantly works"
+        />
+        <Card
+          title="Unbelievable"
+          content="I can't believe how easy it is to use"
+        />
+      </div>
     </div>
   );
 }
@@ -34,19 +35,22 @@ const Card = ({
   return (
     <div
       ref={parentRef}
-      className="relative overflow-hidden before:absolute before:inset-0 before:bg-[radial-gradient(var(--spotlight-size)_circle_at_var(--x)_var(--y),var(--spotlight-color-stops))] rounded-[--radius] bg-white/10 p-2 [--radius:theme(borderRadius.2xl)]"
       style={{
         //@ts-ignore : This is a valid css variable
         "--x": `${x}px`,
         "--y": `${y}px`,
-        "--spotlight-color-stops": "#d4d4d4,transparent",
-        "--spotlight-size": "350px",
-        "--radius": "1rem",
       }}
+      className="relative overflow-hidden rounded-[20px] bg-white/10 p-2"
     >
-      <div className="absolute inset-px rounded-[calc(var(--radius)-1px)] dark:bg-neutral-800 bg-neutral-100" />
-
-      <div className="relative rounded-[calc(var(--radius)-4px)] bg-neutral-200 dark:bg-neutral-900 h-40" />
+      <div
+        className="size-40 rounded-full blur-3xl absolute top-[var(--y)] left-[var(--x)] -translate-x-1/2 -translate-y-1/2"
+        style={{
+          background:
+            "linear-gradient(135deg, #3BC4F2, #7A69F9,#F26378,#F5833F)",
+        }}
+      />
+      <div className="absolute inset-px rounded-[19px] dark:bg-neutral-800/70 bg-neutral-100/70" />
+      <div className="relative rounded-2xl bg-white dark:bg-neutral-900/50 h-40 backdrop-blur-xl" />
       <div className="relative px-4 pb-2 pt-4">
         <h3 className="text-lg font-semibold text-neutral-800 dark:text-neutral-300">
           {title}
--- src/app/(components)/other/cursors/page.tsx
+++ src/app/(components)/other/cursors/page.tsx
@@ -1,6 +1,7 @@
 import FullComponent from "#/src/components/full-component";
 import { getFileContentAsString } from "#/src/utils/get-file-content-as-string";
 import DynamicCardsVariant1 from "./dynamic-cards/variant1";
+import DynamicCardsVariant2 from "./dynamic-cards/variant2";
 import FollowCursorVariant1 from "./follow-cursor/variant1";
 
 const BASE_COMPONENT_PATH =
@@ -25,10 +26,18 @@ export default async function Page() {
         description="A cursor following effect that can be used in any project with any artisitic style"
       />
       <FullComponent
-        size="sm"
+        size="md"
         componentList={[
           {
-            variantName: "Variant 1",
+            variantName: "Gradient background",
+            component: <DynamicCardsVariant2 />,
+            code: await getFileContentAsString({
+              componentSlug: "cursors",
+              variantName: "dynamic-cards/variant2",
+            }),
+          },
+          {
+            variantName: "Border only",
             component: <DynamicCardsVariant1 />,
             code: await getFileContentAsString({
               componentSlug: "cursors",
@@ -37,8 +46,8 @@ export default async function Page() {
           },
         ]}
         isIframed={false}
-        title="Cursor following effect"
-        description="A cursor following effect that can be used in any project with any artisitic style"
+        title="Grid with cursor following border"
+        description="A grid of cards with a cursor following border effect"
       />
     </>
   );
