d0a9318a0c953121d000a5c37da41f18ee2d878d
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-08-08 01:58:23 +0200

    :sparkles: Improve home page with card preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,35 @@
 import Link from "next/link";
 import { componentCategories } from "#/src/lib/component-categories";
 import GradientContainer from "../ui/gradient-container";
+import { getCategoryHref } from "../utils/get-component-href";
+import { MainMenuCard } from "./card";
 
 export default function Page() {
   return (
     <div className="space-y-8 px-4 my-8">
-      <h1 className="font-medium dark:text-neutral-300 text-neutral-600 text-3xl tracking-tight">
-        CuiCui - Copy Paste quality React component
-      </h1>
-      <p className="text-sm text-neutral-500">
+      <h1 className="header-1">CuiCui - Copy Paste quality React component</h1>
+      <p className="caption-md ">
         CuiCui is a modern UI copy paste library that helps you build beautiful
         websites and applications. Its name "CuiCui" is for Quick UI.
       </p>
       <div className="space-y-10 text-white">
+        <h2 className="header-2">Explore the component categories</h2>
         {componentCategories.map((section) => (
           <div key={section.name} className="space-y-5">
-            <p className="font-semibold text-neutral-400 text-xs uppercase tracking-wider">
-              {section.name}
-            </p>
+            <h3 className="uppercase-title">{section.name}</h3>
 
             <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
               {section.items.map((item) => {
                 return (
                   <Link
-                    href={`/${item.slug}`}
+                    href={getCategoryHref({
+                      parentSlug: section.slug,
+                      item: item,
+                    })}
                     key={item.name}
-                    className="group block rounded-lg"
+                    className="group hover:scale-[1.01] hover:-translate-y-[2px] transition-all active:scale-95"
                   >
-                    <GradientContainer
-                      rounded="sm"
-                      classNameChild="z-30 bg-white"
-                      classNameParent="overflow-hidden"
-                      classNameBlur="z-20 opacity-0 blur-md group-hover:opacity-5 transition-all duration-150 ease-in-out"
-                      background="solid"
-                    >
-                      <div className=" flex gap-2 flex-col p-2 group-hover:opacity-80 transition-opacity">
-                        <h4 className="font-medium dark:text-neutral-300 text-neutral-700 group-hover:translate-x-1 transition-transform">
-                          {item.name}
-                        </h4>
-
-                        {item.description ? (
-                          <p className="text-neutral-500 text-sm">
-                            {item.description}
-                          </p>
-                        ) : null}
-                      </div>
-                    </GradientContainer>
+                    <MainMenuCard item={item} />
                   </Link>
                 );
               })}
