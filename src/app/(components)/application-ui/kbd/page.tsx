c90183d9bb00a228f67b9f85fb75b1eee2b98361
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-08-08 01:59:48 +0200

    🚧 Add coming soon categories

---
+++ src/app/(components)/application-ui/kbd/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ComingSoonCard from "#/src/components/coming-soon";
+
+export default function Page() {
+  return (
+    <div>
+      <ComingSoonCard />
+    </div>
+  );
+}
