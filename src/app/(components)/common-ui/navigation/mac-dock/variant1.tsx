11a91e708ae3ebcb8b17f9dad5e0e1d9c7a1c01a
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-08-09 15:55:45 +0200

    Add-macOs-inspired-dock (#7)
    * 🔧 Allow Image from every sources
    
    * :sparkles: add mackOs dock navigation
    
    * :lipstick: Improve main menu card component

---
+++ src/app/(components)/common-ui/navigation/mac-dock/variant1.tsx
@@ -0,0 +1,86 @@
+import Image, { type StaticImageData } from "next/image";
+import Link from "next/link";
+import { cn } from "#/src/utils/cn";
+
+export default function Dock() {
+  return (
+    <div className="relative w-full">
+      <div className="max-w-full pt-4 mx-auto absolute bottom-0 left-1/2 -translate-x-1/2">
+        <div className="relative ">
+          <div className="absolute bottom-0 left-0 right-0 h-[80px] max-w-full rounded-3xl border dark:border-gray-600/60 dark:bg-gray-800/60 bg-gray-200/60 border-gray-200/60 z-10 pointer-events-none shadow-sm" />
+          <div className="rounded-3xl flex items-end pl-2 overflow-x-auto">
+            <AppIcon
+              imgSrc={
+                "https://cdn.dribbble.com/users/6569/screenshots/17730687/linear_share_dribbble_4x.png"
+              }
+            />
+            <AppIcon
+              imgSrc={
+                "https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png"
+              }
+              withoutBackground
+            />
+            <Link href="https://www.modul.day" target="_blank">
+              <AppIcon
+                imgSrc={
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShVv7zoLmVfsPGPCSZzAH4sU8VY5A1ByzvHQ&s"
+                }
+              />
+            </Link>
+            <AppIcon
+              imgSrc={
+                "https://www.01net.com/app/uploads/2021/01/MEA-firefox-1.jpg"
+              }
+            />
+
+            <AppIcon
+              imgSrc={
+                "https://logo-marque.com/wp-content/uploads/2020/12/Discord-Logo.png"
+              }
+              withoutBackground
+            />
+            {/* <ScrollAreaPrimitive.ScrollAreaScrollbar
+                className="h-1 bg-gray-300 dark:bg-gray-600/60 rounded-b-lg"
+                orientation="horizontal"
+                >
+                <ScrollAreaPrimitive.ScrollAreaThumb className="h-full bg-gray-400 dark:bg-gray-600/40 rounded-lg" />
+                </ScrollAreaPrimitive.ScrollAreaScrollbar>
+                </ScrollAreaPrimitive.Viewport>
+                </ScrollAreaPrimitive.Root> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function AppIcon({
+  imgSrc,
+  withoutBackground,
+}: {
+  imgSrc: string | StaticImageData;
+  withoutBackground?: boolean;
+}) {
+  return (
+    <div className=" p-2 pl-0 grid place-items-center cursor-pointer group transition-all duration-700  z-20 w-fit">
+      <div
+        className={cn(
+          "size-16 group-hover:size-[4.75rem] transition-all duration-200 rounded-2xl inline pointer-events-none z-20 overflow-hidden group-hover:shadow-sm  shadow-inner bg-white dark:bg-gray-800",
+
+          withoutBackground ? "p-2" : "",
+        )}
+      >
+        <Image
+          src={imgSrc}
+          alt="dock"
+          className={cn(
+            "size-full",
+            withoutBackground ? "object-contain" : "object-cover",
+          )}
+          width={96}
+          height={96}
+        />
+      </div>
+    </div>
+  );
+}
--- src/app/(components)/common-ui/navigation/page.tsx
+++ src/app/(components)/common-ui/navigation/page.tsx
@@ -1,32 +1,52 @@
 import FullComponent from "#/src/components/full-component";
 import { getFileContentAsString } from "#/src/utils/get-file-content-as-string";
+import Dock from "./mac-dock/variant1";
 import { VercelNavigationVariant1 } from "./vercel-navigation/variant1";
 import { VercelNavigationVariant2 } from "./vercel-navigation/variant2";
 
 export default async function Page() {
   return (
-    <FullComponent
-      size="sm"
-      componentList={[
-        {
-          variantName: "On hover effect",
-          component: <VercelNavigationVariant1 />,
-          code: await getFileContentAsString({
-            componentSlug: "navigation",
-            variantName: "/vercel-navigation/variant1",
-          }),
-        },
-        {
-          variantName: "On click effect",
-          component: <VercelNavigationVariant2 />,
-          code: await getFileContentAsString({
-            componentSlug: "navigation",
-            variantName: "/vercel-navigation/variant2",
-          }),
-        },
-      ]}
-      title="Vercel Navigation"
-      description="A smooth and simple navigation bar inspired by Vercel one, with modern transitions and animations."
-    />
+    <>
+      <FullComponent
+        size="sm"
+        componentList={[
+          {
+            variantName: "On hover effect",
+            component: <VercelNavigationVariant1 />,
+            code: await getFileContentAsString({
+              componentSlug: "navigation",
+              variantName: "/vercel-navigation/variant1",
+            }),
+          },
+          {
+            variantName: "On click effect",
+            component: <VercelNavigationVariant2 />,
+            code: await getFileContentAsString({
+              componentSlug: "navigation",
+              variantName: "/vercel-navigation/variant2",
+            }),
+          },
+        ]}
+        title="Vercel Navigation"
+        description="A smooth and simple navigation bar inspired by Vercel one, with modern transitions and animations."
+      />
+      <FullComponent
+        size="sm"
+        componentList={[
+          {
+            variantName: "variant1",
+            component: <Dock />,
+            code: await getFileContentAsString({
+              componentSlug: "navigation",
+              variantName: "/mac-dock/variant1",
+            }),
+          },
+        ]}
+        title="MacOS Dock"
+        description="A navigation menu with smooth animation as it is done on macOS but in a web browser!"
+        isIframed={false}
+        inspiration="MacOS Dock"
+      />
+    </>
   );
 }
