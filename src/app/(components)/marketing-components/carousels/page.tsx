02dbd6c7f37987223b84767143f28d98f09dbef1
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-08-10 04:59:55 +0200

    :sparkles: add carousel

---
+++ src/app/(components)/marketing-components/carousels/page.tsx
@@ -0,0 +1,26 @@
+import FullComponent from "#/src/components/full-component";
+import { getFileContentAsString } from "#/src/utils/get-file-content-as-string";
+import { CarouselCylindricalVariant1 } from "./cylindric-3d-carousel/variant1";
+
+export default async function Page() {
+  return (
+    <>
+      <FullComponent
+        size="xs"
+        componentList={[
+          {
+            variantName: "variant 1",
+            component: <CarouselCylindricalVariant1 />,
+            code: await getFileContentAsString({
+              componentSlug: "carousels",
+              variantName: "cylindric-3d-carousel/variant1",
+            }),
+          },
+        ]}
+        isIframed={false}
+        title="Cylindric 3D Carousel"
+        description="A 3D carousel that rotates in a cylindrical way. It's perfect for showcasing multiple images or products."
+      />
+    </>
+  );
+}
