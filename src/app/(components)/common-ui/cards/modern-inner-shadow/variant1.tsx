bd0c3ba5db8915a8dbfc77d3f826dc3a338673f5
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-08-10 04:59:03 +0200

    :sparkles: add simple modern card

---
+++ src/app/(components)/common-ui/cards/modern-inner-shadow/variant1.tsx
@@ -0,0 +1,25 @@
+import { cn } from "#/src/utils/cn";
+
+export function ModernInnerShadowCardVariant1() {
+  return (
+    <div
+      className={cn(
+        "rounded-2xl border text-card-foreground p-6 max-w-sm mx-auto group dark:border-white/10 border-neutral-500/10  ",
+        "dark:shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset]",
+        "hover:scale-[1.01] transition-transform transform-gpu group",
+        "bg-gray-50 dark:bg-neutral-800/80",
+      )}
+    >
+      <div className="h-40 text-xl text-neutral-500 tracking-tighter font-bold text-neutral-500/20 text-center translate-y-12 -rotate-6 ">
+        To replace with some content
+      </div>
+      <h6 className="mb-2 font-semibold tracking-tighter dark:text-gray-300 text-gray-600 text-xl group-hover:scale-90 origin-left transition-all transform-gpu">
+        Damn good card
+      </h6>
+      <p className=" dark:text-gray-400 tracking-tight text-sm">
+        This card is better in dark mode. It has a nice inner shadow and a nice
+        border. It's a good modern card.
+      </p>
+    </div>
+  );
+}
---
+++ src/app/(components)/common-ui/cards/page.tsx
@@ -0,0 +1,26 @@
+import FullComponent from "#/src/components/full-component";
+import { getFileContentAsString } from "#/src/utils/get-file-content-as-string";
+import { ModernInnerShadowCardVariant1 } from "./modern-inner-shadow/variant1";
+
+export default async function Page() {
+  return (
+    <>
+      <FullComponent
+        size="sm"
+        componentList={[
+          {
+            variantName: "Variant 1",
+            component: <ModernInnerShadowCardVariant1 />,
+            code: await getFileContentAsString({
+              componentSlug: "cards",
+              variantName: "modern-inner-shadow/variant1",
+            }),
+          },
+        ]}
+        isIframed={false}
+        title="Modern inner shadow card"
+        description="A card that showcases a set of tools that you use to create your product."
+      />
+    </>
+  );
+}
