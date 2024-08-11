845ad9d3ec5ad9fc47a55b72ea0c1bde348c4d1d
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-08-11 17:00:30 +0200

    :truck: Add & move Shadcn UI based components

--- src/ui/shadcn/scrollarea.tsx
+++ src/ui/shadcn/scrollarea.tsx
@@ -2,7 +2,7 @@
 
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
 import * as React from "react";
-import { cn } from "../utils/cn";
+import { cn } from "../../utils/cn";
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
