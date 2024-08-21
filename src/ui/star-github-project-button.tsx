dc6f661a8a7707e50c55b376c6b3e10836a6ad52
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-08-21 19:07:37 +0200

    :art: refactor github link to const variable

--- src/ui/star-github-project-button.tsx
+++ src/ui/star-github-project-button.tsx
@@ -1,11 +1,12 @@
 import { GithubIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { CUICUI_GITHUB_URL } from "../lib/site.const";
 import GradientContainer from "./gradient-container";
 
 export default function StarGithubProjectButton() {
   return (
-    <Link href="https://github.com/damien-schneider/cuicui">
+    <Link href={CUICUI_GITHUB_URL}>
       <GradientContainer
         rounded="sm"
         classNameChild="group-hover:opacity-80 transition-opacity duration-150 ease-in-out px-4 py-2 flex gap-2 items-center text-sm text-neutral-500 font-medium"
