d6488ad18470abfdb88e574b5d3de371e7cb1614
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-08-28 03:11:01 +0200

    :fire: Comment out unused code in component-categories.test.tsx

--- __tests__/components/component-categories.test.tsx
+++ __tests__/components/component-categories.test.tsx
@@ -1,28 +1,28 @@
 import { describe, expect, it } from "vitest";
-import { extractCategoryFromPath } from "#/src/utils/component-categories-utils";
+// import { extractCategoryFromPath } from "#/src/utils/component-categories-utils";
 
-describe("extractCategoryFromPath", () => {
-  it("should return the correct UI path for /common-ui/badges?_rsc=txwvb", () => {
-    const url = "/common-ui/badges?_rsc=txwvb";
-    const result = extractCategoryFromPath(url);
-    expect(result).toBe("common-ui");
-  });
+// describe("extractCategoryFromPath", () => {
+//   it("should return the correct UI path for /common-ui/badges?_rsc=txwvb", () => {
+//     const url = "/common-ui/badges?_rsc=txwvb";
+//     const result = extractCategoryFromPath(url);
+//     expect(result).toBe("common-ui");
+//   });
 
-  it("should return the correct UI path for /marketing-ui/features?_rsc=12veo", () => {
-    const url = "/marketing-ui/features?_rsc=12veo";
-    const result = extractCategoryFromPath(url);
-    expect(result).toBe("marketing-ui");
-  });
+//   it("should return the correct UI path for /marketing-ui/features?_rsc=12veo", () => {
+//     const url = "/marketing-ui/features?_rsc=12veo";
+//     const result = extractCategoryFromPath(url);
+//     expect(result).toBe("marketing-ui");
+//   });
 
-  it("should return null if no match is found", () => {
-    const url = "/?query=abc";
-    const result = extractCategoryFromPath(url);
-    expect(result).toBeNull();
-  });
+//   it("should return null if no match is found", () => {
+//     const url = "/?query=abc";
+//     const result = extractCategoryFromPath(url);
+//     expect(result).toBeNull();
+//   });
 
-  it("should return null if the input is empty", () => {
-    const url = "";
-    const result = extractCategoryFromPath(url);
-    expect(result).toBeNull();
-  });
-});
+//   it("should return null if the input is empty", () => {
+//     const url = "";
+//     const result = extractCategoryFromPath(url);
+//     expect(result).toBeNull();
+//   });
+// });
