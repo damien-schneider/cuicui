c581d6ecba1dd0ee6fe786dd4ec2f915fa578264
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-09-01 17:39:59 +0200

    :fire: Remove unused borderRadius styles in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -45,11 +45,6 @@ export default {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       fontFamily: {
         sans: ["var(--font-dm-sans)", "sans-serif"],
       },
