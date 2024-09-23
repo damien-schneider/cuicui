c239c73659ca7e68ae28fa84fbfeeba5888c683d
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-09-23 13:50:55 +0200

    :sparkles: add code block component & inline component

---
+++ src/ui/cuicui/application-ui/code/advanced-code-block/advanced-code-block.component.tsx
@@ -0,0 +1,27 @@
+import type { ComponentType } from "#/src/lib/types/component";
+import PreviewAdvancedCodeBlock from "#/src/ui/cuicui/application-ui/code/advanced-code-block/preview-advanced-code-block";
+import PreviewAdvancedCodeBlockTabVariant from "#/src/ui/cuicui/application-ui/code/advanced-code-block/preview-advanced-code-block-tab-variant";
+
+export const advancedCodeBlockComponent: ComponentType = {
+  isIframed: false,
+  sizePreview: "sm",
+  slug: "advanced-code-block",
+  variantList: [
+    {
+      name: "Default",
+      component: <PreviewAdvancedCodeBlock />,
+      slugPreviewFile: "preview-advanced-code-block",
+      slugComponentFile: "advanced-code-block",
+    },
+    {
+      name: "With tabs",
+      component: <PreviewAdvancedCodeBlockTabVariant />,
+      slugPreviewFile: "preview-advanced-code-block-tab-variant",
+      slugComponentFile: "advanced-code-block",
+    },
+  ],
+  title: "Advanced Code Block",
+  description: "A code snippet component with a copy to clipboard button.",
+  inspiration: "Clerk",
+  inspirationLink: "https://clerk.com/docs/quickstarts/nextjs",
+};
---
+++ src/ui/cuicui/application-ui/code/advanced-code-block/code-highlighter.tsx
@@ -0,0 +1,24 @@
+"use server";
+import { type BundledLanguage, type BundledTheme, codeToHtml } from "shiki";
+import { cn } from "#/src/utils/cn";
+import "./style.css";
+type ShikiProps = {
+  code: string;
+  lang: BundledLanguage;
+  theme: BundledTheme;
+  className?: string;
+};
+export async function ShikiCode({ code, lang, theme, className }: ShikiProps) {
+  const html = await codeToHtml(code, {
+    lang,
+    theme,
+  });
+
+  return (
+    <div
+      className={cn("*:bg-transparent", className)}
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: <Done server side, no worries>
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  );
+}
