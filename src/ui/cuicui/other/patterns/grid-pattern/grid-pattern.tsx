e0b3036a92f6a366701631061d21195e955fed28
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-10-15 01:52:43 +0200

    Use click outside & noise & border trail & grid pattern (#46)
    * ✨ Add border trail & noise creative effect component
    
    * ✨ Add useClickOutsideCategory to hooksSection
    
    * ✨ Add grid pattern component to patterns category
    
    * ✨ Add CSS Selectors & noise generation tool to tools section

---
+++ src/ui/cuicui/other/patterns/grid-pattern/grid-pattern.tsx
@@ -0,0 +1,5 @@
+export default function GridPattern() {
+  return (
+    <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:35px_34px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
+  );
+}
