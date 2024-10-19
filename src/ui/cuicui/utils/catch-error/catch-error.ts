d7c8c2ef34bcaa1be1ae5c14e3bfde04783eab22
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-10-19 09:24:23 +0200

    :sparkles: add catch-error utils

---
+++ src/ui/cuicui/utils/catch-error/catch-error.ts
@@ -0,0 +1,11 @@
+export async function catchError<T>(
+  promise: Promise<T>,
+): Promise<[undefined, T] | [Error]> {
+  return promise
+    .then((data) => {
+      return [undefined, data] as [undefined, T];
+    })
+    .catch((error) => {
+      return [error];
+    });
+}
