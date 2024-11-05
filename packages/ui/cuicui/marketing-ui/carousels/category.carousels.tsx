a2d64ad0a08afafea5591c354fc4fb4407009949
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-11-05 15:40:49 +0100

    🚚 move sections & replace useless tsx by ts

---
+++ packages/ui/cuicui/marketing-ui/carousels/category.carousels.tsx
@@ -0,0 +1,50 @@
+import { CarouselCylindricalVariant1 } from "@/cuicui/marketing-ui/carousels/cylindric-3d-carousel/variant1";
+import { SimpleCarouselPreview } from "@/cuicui/marketing-ui/carousels/simple-carousel/simple-carousel-preview";
+import type { CategoryType } from "@/lib/types/component";
+import { GalleryHorizontalIcon } from "lucide-react";
+
+export const carouselsCategory: CategoryType = {
+	slug: "carousels",
+	name: "Carousels",
+	description: "Showcase your product testimonials with style",
+	releaseDateCategory: new Date("2024-08-10"),
+	icon: GalleryHorizontalIcon,
+	previewCategory: {
+		component: <SimpleCarouselPreview />,
+		previewScale: 0.5,
+	},
+	componentList: [
+		{
+			name: "Simple Carousel",
+			description:
+				"A simple carousel that allows you to navigate through multiple images or products.",
+			sizePreview: "md",
+			slug: "simple-carousel",
+			lastUpdatedDateComponent: new Date("2024-09-10"),
+
+			variantList: [
+				{
+					name: "Simple Carousel",
+					component: <SimpleCarouselPreview />,
+					slugPreviewFile: "simple-carousel-preview",
+					slugComponentFile: "simple-carousel",
+				},
+			],
+		},
+		{
+			name: "Cylindric 3D Carousel",
+			lastUpdatedDateComponent: new Date("2024-08-10"),
+			description:
+				"A 3D carousel that rotates in a cylindrical way. It's perfect for showcasing multiple images or products.",
+			sizePreview: "md",
+			slug: "cylindric-3d-carousel",
+			variantList: [
+				{
+					name: "Cylindrical Carousel",
+					component: <CarouselCylindricalVariant1 />,
+					slugPreviewFile: "variant1",
+				},
+			],
+		},
+	],
+};
---
+++ packages/ui/cuicui/marketing-ui/testimonials/category.testimonials.tsx
@@ -0,0 +1,56 @@
+import { gradientQnaComponent } from "@/cuicui/marketing-ui/testimonials/gradient-qna/component.gradient-qna";
+import MarqueeVariant1 from "@/cuicui/marketing-ui/testimonials/marquee/variant1";
+import MarqueeVariant2 from "@/cuicui/marketing-ui/testimonials/marquee/variant2";
+import { TestimonialsVariant1 } from "@/cuicui/marketing-ui/testimonials/testimonials-component/variant1";
+import type { CategoryType } from "@/lib/types/component";
+import { BookOpenTextIcon } from "lucide-react";
+
+export const testimonialsCategory: CategoryType = {
+	slug: "testimonials",
+	name: "Testimonials",
+	description: "Showcase your product testimonials with style",
+	releaseDateCategory: new Date("2024-07-17"),
+	icon: BookOpenTextIcon,
+	previewCategory: {
+		component: <MarqueeVariant1 />,
+		previewScale: 0.75,
+	},
+	componentList: [
+		{
+			isResizable: true,
+			lastUpdatedDateComponent: new Date("2024-07-17"),
+			sizePreview: "md",
+			slug: "testimonials-component",
+			name: "Testimonials",
+			description: "A simple testimonials component with a slider on click",
+			variantList: [
+				{
+					name: "Testimonials Slider",
+					slugPreviewFile: "variant1",
+					component: <TestimonialsVariant1 />,
+				},
+			],
+		},
+		{
+			lastUpdatedDateComponent: new Date("2024-07-17"),
+			sizePreview: "sm",
+			slug: "marquee",
+			name: "Marquee",
+			description:
+				"A simple marquee component with two variants, horizontal and vertical.",
+			variantList: [
+				{
+					name: "Horizontal Marquee",
+					component: <MarqueeVariant1 />,
+					slugPreviewFile: "variant1",
+				},
+				{
+					name: "Vertical Marquee",
+					component: <MarqueeVariant2 />,
+					slugPreviewFile: "variant2",
+				},
+			],
+		},
+		gradientQnaComponent,
+	],
+};
