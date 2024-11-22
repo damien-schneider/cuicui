ba856ae0fce06c317c77d07a32db49c2b73d743c
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-11-22 23:22:40 +0100

    :sparkles: add sticky footer components

---
+++ packages/ui/cuicui/marketing-ui/footer/sticky-footer/component.ts
@@ -0,0 +1,19 @@
+import type { ComponentType } from "@/lib/types/component";
+import { PreviewStickyFooter } from "@/cuicui/marketing-ui/footer/sticky-footer/preview.sticky-footer";
+
+export const footerComponent: ComponentType = {
+	sizePreview: "md",
+	slug: "sticky-footer",
+	name: "Sticky Footer",
+	description: "A sticky footer that stays at the bottom of the page.",
+	isIframed: true,
+	variantList: [
+		{
+			name: "Fixed positionned",
+			component: PreviewStickyFooter,
+			slugPreviewFile: "preview.sticky-footer",
+		},
+	],
+};
+
+export default footerComponent;
