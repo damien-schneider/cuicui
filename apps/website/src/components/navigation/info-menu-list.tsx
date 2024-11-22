1a25e07341cb2fc66fa1bb76549d6cd6473168bb
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-11-22 23:21:43 +0100

    :sparkles: add functionality to retrieve the latest changelog date and update info menu to indicate new changes

--- apps/website/src/components/navigation/info-menu-list.tsx
+++ apps/website/src/components/navigation/info-menu-list.tsx
@@ -1,31 +1,38 @@
+import { getLatestChangelogDate } from "#/src/app/(site)/changelog/get-latest-release-date";
 import { NavigationAnimatedBackground } from "#/src/components/navigation/navigation-animated-background";
 import {
-	GlobalNavItem,
-	SectionWrapper,
+  GlobalNavItem,
+  SectionWrapper,
 } from "#/src/components/navigation/navigation-item";
 import { firstMenuSection } from "#/src/lib/first-menu-section";
-import React from "react";
 
-export default function InfoMenuList() {
-	return (
-		<SectionWrapper
-			sectionSlug=""
-			name={firstMenuSection.name}
-			className="w-full mt-4"
-		>
-			<NavigationAnimatedBackground>
-				{firstMenuSection.categoryList.map((category, _index) => (
-					<li className="block" data-id={category.slug} key={category.slug}>
-						<GlobalNavItem
-							Icon={null}
-							href={category.href ?? `/${category.slug}`}
-							isMobile={false}
-							name={category.name}
-							target={category.href ? "newWindow" : "sameWindow"}
-						/>
-					</li>
-				))}
-			</NavigationAnimatedBackground>
-		</SectionWrapper>
-	);
+export default async function InfoMenuList() {
+  const latestChangelogDate = await getLatestChangelogDate();
+  const today = new Date();
+  // is New if latest changelog date is within 7 days
+  const isNew = latestChangelogDate
+    ? latestChangelogDate > new Date(today.setDate(today.getDate() - 1))
+    : false;
+  return (
+    <SectionWrapper
+      sectionSlug=""
+      name={firstMenuSection.name}
+      className="w-full mt-4"
+    >
+      <NavigationAnimatedBackground>
+        {firstMenuSection.categoryList.map((category, _index) => (
+          <li className="block" data-id={category.slug} key={category.slug}>
+            <GlobalNavItem
+              Icon={null}
+              href={category.href ?? `/${category.slug}`}
+              isMobile={false}
+              name={category.name}
+              tag={category.slug === "changelog" && isNew ? "New" : undefined}
+              target={category.href ? "newWindow" : "sameWindow"}
+            />
+          </li>
+        ))}
+      </NavigationAnimatedBackground>
+    </SectionWrapper>
+  );
 }
