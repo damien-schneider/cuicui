4de290cfeb6a120554dfbacfb79e0b33c8b944c2
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-11-23 14:44:48 +0100

    V0.3.4 (#58)
    * 🔧 update biome configuration to include noUndeclaredDependencies and format ignore list
    
    * ✨ add trigger-on-scroll component with animated number functionality
    
    * 📝 update changelog for the trigger on scroll component

---
+++ packages/ui/cuicui/marketing-ui/statistics/trigger-on-scroll/trigger-on-scroll.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { cn } from "@/cuicui/utils/cn/cn";
+import NumberFlow from "@number-flow/react";
+import { useState, useRef, useEffect } from "react";
+import { useInView } from "framer-motion";
+
+type NumberProps = number | `${number}`;
+
+export function TriggerOnScroll({
+  className,
+  children,
+  delayInMs = 100,
+}: { className?: string; children: NumberProps; delayInMs?: number }) {
+  const [number, setNumber] = useState<NumberProps>(0);
+  const ref = useRef(null);
+  const isInView = useInView(ref, { margin: "0px 0px -100px 0px" });
+
+  useEffect(() => {
+    if (isInView) {
+      setTimeout(() => setNumber(children), delayInMs);
+    } else {
+      setNumber(0);
+    }
+  }, [isInView, children, delayInMs]);
+
+  return (
+    <NumberFlow
+      ref={ref}
+      value={Number(number)}
+      isolate={true}
+      spinTiming={{ duration: 1200, easing: "ease-in-out" }}
+      className={cn(
+        "z-20 dark:text-neutral-300 text-neutral-700 text-4xl font-medium tracking-tighter",
+        className,
+      )}
+    />
+  );
+}
