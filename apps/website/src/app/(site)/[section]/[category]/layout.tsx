8880d7b7546fcdef8353b425e3e5584bb25eaca7
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-12-13 05:23:24 +0100

    🔄 refactor generateMetadata and CategoryLayout to improve type definitions and simplify props structure

--- apps/website/src/app/(site)/[section]/[category]/layout.tsx
+++ apps/website/src/app/(site)/[section]/[category]/layout.tsx
@@ -7,14 +7,15 @@ import { NEXT_PUBLIC_SITE_URL } from "#/src/lib/site.const";
 import { findCategoryBySlug } from "#/src/utils/section-category-components-utils/find-category-by-slug";
 import { findSectionBySlug } from "#/src/utils/section-category-components-utils/find-section-by-slug";
 
-export async function generateMetadata({
-  params,
-}: {
+type Props = {
+  children: JSX.Element;
   params: Promise<{
     section: string;
     category: string;
   }>;
-}): Promise<Metadata> {
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { section: sectionParam, category: categoryParam } = await params;
   const section = findSectionBySlug(sectionParam);
   if (!section) {
@@ -58,16 +59,7 @@ export async function generateMetadata({
   return {};
 }
 
-export default async function CategoryLayout({
-  params,
-  children,
-}: {
-  children: JSX.Element;
-  params: Promise<{
-    section: string;
-    category: string;
-  }>;
-}) {
+export default async function CategoryLayout({ params, children }: Props) {
   const { section: sectionParam, category: categoryParam } = await params;
 
   const sectionInList = findSectionBySlug(sectionParam);
--- apps/website/src/app/(site)/[section]/layout.tsx
+++ apps/website/src/app/(site)/[section]/layout.tsx
@@ -10,7 +10,6 @@ type Props = {
   children: ReactNode;
   params: Promise<{
     section: string;
-    category: string;
   }>;
 };
 
